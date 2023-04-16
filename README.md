069738ca7d0702c4a4f462d7375e5638db341f8a
David Konsumer <konsumer@jetboystudio.com>
2023-04-15 19:35:37 -0700

    scaffolding

---
+++ README.md
@@ -0,0 +1,12 @@
+# Glitchy Video
+
+Glitch video to soundcloud music EQ data
+
+## Demo
+
+You can see it in action, [here](https://vidglitch.surge.sh)
+
+## Installation
+
+* Install dependencies for web-server with `npm install`
+* Start a local webserver with `npm start`
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,80 @@
-import React from 'react'
+import React, { Suspense, useMemo, useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
-
-import { Button } from 'flowbite-react'
-
+import { Canvas } from '@react-three/fiber'
+import { useAspect, useVideoTexture, useTexture } from '@react-three/drei'
+import { FullScreen, useFullScreenHandle } from 'react-full-screen'
 import '@/site.css'
-import ButtonTheme from '@/components/ButtonTheme'
+
+import vsBasic from './shaders/basic.vs.glsl?raw'
+import fsTvBass from './shaders/tv-bass.fs.glsl?raw'
+
+// TODO: see this for example of applying shader:
+// https://codesandbox.io/s/react-three-fiber-custom-geometry-with-fragment-shader-material-vxswf
+
+// more ideas here: https://youtu.be/e2ntx-fyXaE
+
+function VideoMaterial ({ url }) {
+  const texture = useVideoTexture(url)
+  return <meshBasicMaterial map={texture} toneMapped={false} />
+}
+
+function FallbackMaterial ({ url }) {
+  const texture = useTexture(url)
+  return <meshBasicMaterial map={texture} toneMapped={false} />
+}
+
+/*
+uniform sampler2D tDiffuse;
+uniform float time; // steadily increasing float passed in
+uniform float eqL[32]; // EQ left
+uniform float eqR[32]; // EQ right
+uniform float speed; // distortion vertical travel speed
+*/
+
+function Scene ({ video, fallback, width, height }) {
+  const size = useAspect(width, height)
+  const [time, setTime] = useState(0)
+  const [eq, setEq] = useState([
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+  ])
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      setTime(Date.now())
+    }, 100)
+    return () => clearInterval(i)
+  }, [])
+
+  const uniforms = useMemo(
+    () => ({
+      eqL: { value: eq[0] },
+      eqR: { value: eq[1] },
+      speed: { value: 1.0 },
+      time: { value: time }
+    }),
+    [time, eq]
+  )
+
+  return (
+    <mesh scale={size}>
+      <planeGeometry />
+      <Suspense fallback={<FallbackMaterial url={fallback} />}>
+        <VideoMaterial url={video} />
+      </Suspense>
+      <shaderMaterial attach='material' vertexShader={vsBasic} fragmentShader={fsTvBass} uniforms={uniforms} />
+    </mesh>
+  )
+}
 
 function App () {
+  const handle = useFullScreenHandle()
   return (
-    <div className='p-4'>
-      IT WORKS <ButtonTheme />
-    </div>
+    <FullScreen handle={handle}>
+      <Canvas orthographic>
+        <Scene video='/space.mp4' fallback='/space.png' width={640} height={360} />
+      </Canvas>
+    </FullScreen>
   )
 }
 
